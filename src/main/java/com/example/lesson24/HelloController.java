a62acdb54ce1f7649209216274e2642e6470cdb3
asrytikov <asrytikov@gmail.com>
2023-10-14 20:56:46 +0400

    Lesson24

--- src/main/java/com/example/lesson24/HelloController.java
+++ src/main/java/com/example/lesson24/HelloController.java
@@ -1,2 +1,21 @@
-package com.example.lesson24;public class HelloController {
+package com.example.lesson24;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello!";
+    }
+
+    @GetMapping("/ciao")
+    public String ciao(){
+        return "ciao!";
+    }
+
 }
--- src/main/java/com/example/lesson24/controllers/CountryController.java
+++ src/main/java/com/example/lesson24/controllers/CountryController.java
@@ -1,2 +1,31 @@
-package com.example.lesson24.controllers;public class CountryController {
+package com.example.lesson24.controllers;
+
+import com.example.lesson24.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/france")
+    public ResponseEntity<Country> france(){
+        Country country = Country.of("France", 80);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital", "Paris")
+                .body(country);
+    }
+
+    @GetMapping("/all")
+    public List<Country> countries(){
+        Country country1 = Country.of("France", 80);
+        Country country2 = Country.of("Spain", 50);
+        return List.of(country1, country2);
+    }
+
 }
